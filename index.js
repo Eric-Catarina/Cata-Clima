33c7c963bb710a4f73c851fee3d444f95c8d89e3
Eric-Catarina <2020954200@teiacoltec.org>
2022-08-07 14:52:56 -0300

    Integraçã Google places --> Open Weather Funcionando

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
+
 let autocomplete;
 let nomeDoLugar;
 let input = document.getElementById('autocomplete')
+let dadosDoLugarClicado
+let jsonDaCidadeURL
+let nomeDaCidade
 
 function handler(){
-    console.log(autocomplete.getPlace())
+    dadosDoLugarClicado = autocomplete.getPlace()
+    nomeDaCidade = dadosDoLugarClicado.address_components[0].long_name
+    
+    jsonDaCidadeURL = (`https://api.openweathermap.org/data/2.5/weather?q=${nomeDaCidade}&appid=6f0938ac962003085f29f2dd5cefc18d`)
+    
+    $.getJSON(jsonDaCidadeURL, function(jsonDaCidade){
+       console.log(jsonDaCidade)
+    })
+   
+    
+
 }
 
 function RecebeStringDoInputAutocomplete(){
