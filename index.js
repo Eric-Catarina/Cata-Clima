51d025618c74e29a872587042d419a1eb8822f7b
Eric-Catarina <2020954200@teiacoltec.org>
2022-08-12 20:45:05 -0300

    Fahrenheit para Celsius Funcionando

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ let input = document.getElementById('autocomplete')
 let dadosDoLugarClicado
 let jsonDaCidadeURL
 let nomeDaCidade
+let tipoDaTemperatura = "metric"
+let tipoDaTemperaturaCont = 0
+let jsonDaCidadeGlobal
+let jsonDaCidade
 
 function ToggleSeta(){
     var elementoSeta = document.getElementById("seta");
@@ -16,24 +20,62 @@ function ToggleAutoComplete(){
 }
 function AlternaPaginas(){
     var elementosAlternaveis = document.getElementsByClassName("alternavel")
-    
+
     for (const elementoAtual of elementosAlternaveis){
         elementoAtual.classList.toggle("d-none")
     }
 }
+function InsereNomeDaCidade(){
+    document.getElementById("nomeCidade").innerHTML = nomeDaCidade
+}
+function InsereTemperaturas(jsonDaCidade){
+    jsonDaCidade = jsonDaCidade.main
+    let temperatura =  jsonDaCidade.temp
+    let temperaturaArredondada = Math.round(temperatura)
+    document.getElementById("temperatura").innerHTML = temperaturaArredondada + "°"
 
+    let temperaturaMinima = (jsonDaCidade.temp_min) - 1
+    let temperaturaMinimaArredondada = Math.round(temperaturaMinima)
+    document.getElementById("min").innerHTML = temperaturaMinimaArredondada + "°"
+
+    let temperaturaMaxima = (jsonDaCidade.temp_max) + 1
+    let temperaturaMaximaArredondada = Math.round(temperaturaMaxima)
+    document.getElementById("max").innerHTML = temperaturaMaximaArredondada + "°"
+
+
+}
+
+function TrocaTipoTemperatura(){
+    
+
+    tipoDaTemperaturaCont +=1
+    if (tipoDaTemperaturaCont % 2 == 0){
+        tipoDaTemperatura= "metric"
+    }
+    else{
+        tipoDaTemperatura = "imperial"
+    }
+    jsonDaCidadeURL = (`https://api.openweathermap.org/data/2.5/weather?q=${nomeDaCidade}&units=${tipoDaTemperatura}&appid=6f0938ac962003085f29f2dd5cefc18d`)
+    
+    $.getJSON(jsonDaCidadeURL, function(jsonDaCidade){
+        InsereTemperaturas(jsonDaCidade)
+    })
+    
+}
 
 function handler(){
     AlternaPaginas()
 
     dadosDoLugarClicado = autocomplete.getPlace()
     nomeDaCidade = dadosDoLugarClicado.address_components[0].long_name
+    InsereNomeDaCidade()
     
-    jsonDaCidadeURL = (`https://api.openweathermap.org/data/2.5/weather?q=${nomeDaCidade}&appid=6f0938ac962003085f29f2dd5cefc18d`)
+    jsonDaCidadeURL = (`https://api.openweathermap.org/data/2.5/weather?q=${nomeDaCidade}&units=${tipoDaTemperatura}&appid=6f0938ac962003085f29f2dd5cefc18d`)
     
     $.getJSON(jsonDaCidadeURL, function(jsonDaCidade){
-
+        jsonDaCidadeGlobal = jsonDaCidade.main
         console.log(jsonDaCidade)
+        InsereTemperaturas(jsonDaCidade)
     })
     
 
